aadcedefcf2c9a11e75cfff0d59a23e2b9509ad3
akku-18 <akshanshgarg18@gmail.com>
2023-05-05 21:29:40 +0530

    draggable card slider

--- CardSlider/cardSlider.js
+++ CardSlider/cardSlider.js
@@ -1,6 +1,30 @@
 const carousel = document.querySelector(".crousel");
+const arrowBtns =  document.querySelectorAll(".wrapper i");
+const firstCardWidth = document.querySelector(".card").offsetWidth;
+const carouselChildren = [...carousel.children]; 
 
-let isDragging = false, startX, startScrollLeft;
+let isDragging = false, startX, startScrollLeft ;
+
+// // Get number of cards that can fit in carousel at once 
+// let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+
+// // insert copies of last few cards to beginning of carousel for infinite scrolling
+// carouselChildren.slice(-cardPerView).reverse().forEach(card => {
+//     carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
+// })
+
+// // insert copies of first few cards to end of carousel for infinite scrolling
+// carouselChildren.slice(0, cardPerView).forEach(card => {
+//     carousel.insertAdjacentHTML("beforeend", card.outerHTML);
+// })
+
+
+// Add event listernes to arrow buttons
+arrowBtns.forEach(btn => {
+    btn.addEventListener("click",() => {
+        carousel.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;
+    })
+});
 
 const dragStart = (e) => {
     isDragging = true;
